b0783f744965735ec6fd94ebf4fa3d992c781ac3
Ravi Shankar Singh <ravishankarsingh1996@gmail.com>
2018-12-18 17:10:43 +0530

    Splash Screen Added

--- build/app/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/devmins/flutterappsamples/R.java
+++ build/app/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/devmins/flutterappsamples/R.java
@@ -9,7 +9,8 @@ package com.devmins.flutterappsamples;
 
 public final class R {
   public static final class drawable {
-    public static final int launch_background=0x7f010000;
+    public static final int ic_launcher=0x7f010000;
+    public static final int launch_background=0x7f010001;
   }
   public static final class mipmap {
     public static final int ic_launcher=0x7f020000;
